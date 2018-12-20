da44358d921578b35cdde2e06c540fcb09ae84fb
CarterLankford <43147538+CarterLankford@users.noreply.github.com>
2018-12-20 14:08:49 -0600

    Create Algorithm01.java
    My attempt.

---
+++ Algorithm01.java
@@ -0,0 +1,48 @@
+/**
+ * The HowManyOnes class was created to go with Algorithm01_Rawtext
+ * to make the code concepts into an actual class.  
+ */
+class HowManyOnes {
+    public static void main(String[] args) {
+        //Manually creating the array
+        int[] myArray;
+        myArray = new int[17];
+        myArray[0] = 0;
+        myArray[1] = 0;
+        myArray[2] = 1;
+        myArray[3] = 1;
+        myArray[4] = 0;
+        myArray[5] = 0;
+        myArray[6] = 0;
+        myArray[7] = 1;
+        myArray[8] = 0;
+        myArray[9] = 0;
+        myArray[10] = 1;
+        myArray[11] = 1;
+        myArray[12] = 1;
+        myArray[13] = 0;
+        myArray[14] = 1;
+        myArray[15] = 1;
+        myArray[16] = 0;
+ 
+
+        //My idea of how to do this, used notepad and cmd to write/test
+        //Copy and paste into git. 
+        int highestCount = 0;
+        int currentCount = 0;
+
+        for (int x : myArray){ 
+            if (x == 1) {
+                currentCount++;
+            } else if (x == 0) {
+                if (currentCount > 0) {
+                    if (currentCount > highestCount) {
+                        highestCount = currentCount;
+                    }
+                    currentCount = 0; 
+                }
+            }           
+        }
+        System.out.println("output = " + highestCount);
+    }
+}
