09bfb10249f5f1f79a8a65cc8ed1f15ecbf7956e
CarterLankford <calankford@outlook.com>
2018-12-31 14:41:40 -0600

    Made many changes, this is current stopping place

---
+++ FirstExcersise/Square.java
@@ -0,0 +1,64 @@
+/**
+ * The exercise: Design and write a Square class to represent geomtrical square
+ * For your design consider this:
+ *     - What attributes do you need to represent squares?
+ *     - We would like to have methods to get the area and perimeter of the square
+ *     - What other actions/methods do you think that can be added to the square?
+ */
+
+
+
+public class Square {
+    private double side;
+
+    public static void main(String[] args) {
+        Square sq = new Square(); //Create the object
+
+        sq.setSide((double)5.1); //Set the value of the object
+
+	System.out.println(sq.getArea()); //Show result of a function
+    }
+
+    public double getArea() {
+        return side * side; 
+    }
+
+    public double getPerimeter() {
+        return side * 4;
+    }
+    
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side; 
+    }
+
+    private static boolean IsValidValue(double x) {
+        boolean isValidValue = false;
+
+        if (x >= 0){
+            isValidValue = true;
+        }
+
+        return isValidValue;
+    }
+
+}
+
+// change to "public double getArea()", create object and call, read slides. 
+
+
+/**
+ * Square mySquare = new Square(4);
+ * 
+ * System.out.println(mySquare.getArea());
+ * 
+ * //later
+ * try{
+ * 	mySquare = new Square(0);
+ * }catch(IllegalArgumentException e){
+ * ...
+ * }
+ */
