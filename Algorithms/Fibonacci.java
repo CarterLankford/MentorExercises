e8c005405445f4d60f357837f06e4a622c7aaf45
CarterLankford <calankford@outlook.com>
2019-01-10 17:26:58 -0600

    Post mentor meeting, added comments to code

--- Algorithms/Fibonacci.java
+++ Algorithms/Fibonacci.java
@@ -1,46 +1,15 @@
-/*
-TODO:
-Rather than having calculateResult do 
-most of the heavy lifting, create more
-functions to take the load of it. Not 
-sure if setters are a good idea for 
-value or not, will try to see what 
-happens. 
-
-That doesn't make any sense for setters
-class is self contained, only setter
-should be for the initial index request 
-*/
-
-
 class Fibonacci {
 
 	public static void main(String[] args) {
 		// Suggestion
-		// System.out.println(calculateResult(12L)); // 233
-		
-			// System.out.println(calculateResult(1000000) + "\n"); // 144
-			System.out.println(fib(1000000) + "\n"); // 144
+		// System.out.println(calculateResult(12L)); // 233	
+		System.out.println(fib(1000000) + "\n"); // 144
 		// for(int i = 0; i<1000; i++){
 		// }
 	}
 
-
-	//using calculateresult as is (once converted into a static method) figure out the calculation. 
-	// public long calculateResult() {
-	// 	Fibonacci fb = new Fibonacci();
-	// 	int x = 0;	//Setting Counter at 0	
-	// 	while (x <= requestedIndex){			
-	// 		result = fb.getSum(firstValue, secondValue);
-	// 		firstValue = secondValue;
-	// 		secondValue = result;
-	// 		x++;
-	// 		System.out.println( result + " I:" + x);			
-	// 	}
-	// 	return result;
-	// }
-
 	public static long calculateResult(int requestedIndex) {
+		//To find out if value is 0,1  Placed at start to be cheap and not use memory when not needed. 
 		if(requestedIndex < 2){
 			return requestedIndex;
 		}
@@ -54,16 +23,20 @@ class Fibonacci {
 
 			firstNumber = secondNumber;
 			secondNumber = result;
-			// System.out.println("R:" + result + " I:" + currentIndex);
 		}
-		// while (currentIndex <= requestedIndex) {
-		// 	result = firstNumber + secondNumber;
 
-		// 	firstNumber = secondNumber;
-		// 	secondNumber = result;
-		// 	System.out.println("R:" + result + " I:" + currentIndex);
-		// 	currentIndex++;
-		// }
+		//How to do as While loop, converted to for loop since end of index is known
+		/*
+		while (currentIndex <= requestedIndex) {
+			result = firstNumber + secondNumber;
+
+			firstNumber = secondNumber;
+			secondNumber = result;
+			System.out.println("R:" + result + " I:" + currentIndex);
+			currentIndex++;
+		}
+		*/
+		
 
 		return result;
 	}
