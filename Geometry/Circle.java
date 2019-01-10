36c0e0996874fd93354b9756abbb04cc7cac4df9
CarterLankford <calankford@outlook.com>
2019-01-10 11:13:53 -0600

    wrote this in about 4 minutes, made a UML before...UML made this much easier to create

--- Geometry/Circle.java
+++ Geometry/Circle.java
@@ -3,4 +3,33 @@ public class Circle {
     //diameter = 2xradius
     //circumfrence = Pi x diameter = 2xPI x radius where pi=3.14159
     //area = pi x radius(squared)
+
+    private double radius;
+    private double pi = 3.14159;
+
+
+    public static void main(String[] args){
+    	Circle cr = new Circle();
+    	cr.setRadius(1.5);
+
+    	System.out.println(cr.getDiameter());
+    	System.out.println(cr.getArea());
+    	System.out.println(cr.getCircumfrence());
+    }
+
+    public double getDiameter(){
+    	return radius * 2;
+    }
+
+    public double getCircumfrence(){
+    	return ((2 * pi) * radius);
+    }
+
+    public double getArea(){
+    	return pi * (radius * radius);
+    }
+
+    public void setRadius(double radius){
+    	this.radius = radius;
+    }
 }
