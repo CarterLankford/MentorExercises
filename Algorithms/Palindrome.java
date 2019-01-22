ed618dc522be225e9f30bdc23d64dee508551d1f
CarterLankford <calankford@outlook.com>
2019-01-21 22:49:32 -0600

    adding code for algorithm challenge: palindrome

---
+++ Algorithms/Palindrome.java
@@ -0,0 +1,24 @@
+class Palindrome{
+
+	public static void main(String[] args){
+		String strStartingString = "aabbccddccbaa";
+		String strNewString = "";  
+		boolean isPalindrome = false;
+		int b = strStartingString.length() - 1;
+
+		while (b >= 0){
+			//System.out.println(strStartingString.charAt(b));
+			strNewString = strNewString + strStartingString.charAt(b);
+			b--;
+		}
+
+		isPalindrome = strStartingString.equals(strNewString);
+
+		if (isPalindrome){
+			System.out.println(strStartingString + " is a palindrome.");
+		} else {
+			System.out.println(strStartingString + " is not a palindrome.");
+		}
+
+	}
+}
