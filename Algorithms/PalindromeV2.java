864ff1fa25c2d9a9e711ef8b15b67f5bf0b9d2b7
CarterLankford <calankford@outlook.com>
2019-02-04 12:49:02 -0600

    modified palindromeV2 to use the floor method as discussed. need to add comments

--- Algorithms/PalindromeV2.java
+++ Algorithms/PalindromeV2.java
@@ -1,20 +1,18 @@
 class PalindromeV2{
 	public static void main(String[] args){
-		String strStartingString = "aabbccddccbbaa";
+		String strStartingString = "aabbccdccbbaa";
 		boolean isPalindrom = true;
-		int checkLeft = 0; //will be used to check letters on the left side of the string
-		int checkRight = 0; //will be used to check letters on the right side of the string
 		char checkString[] = strStartingString.toCharArray(); 
+		int n = strStartingString.length();
+		int j = (int)Math.floor((double)strStartingString.length() / 2);
 
-        //will start with the outskirts and work its way in, if ever false will fail out and adjust the boolean value
-		for(checkLeft = 0, checkRight = strStartingString.length() - 1; checkLeft < checkRight; checkLeft++, checkRight--){
-			if (checkString[checkLeft] != checkString[checkRight]) {
-				isPalindrom = false;
+		for (int i = 0; i < j; i++){
+			int last = n - 1 - i;
+			if (checkString[i] != checkString[last]){
+				isPalindrom = !isPalindrom;
 				break;
 			}
 		}
-
 		System.out.println("Is Palindrome: " + isPalindrom);
-		
 	}
 }
