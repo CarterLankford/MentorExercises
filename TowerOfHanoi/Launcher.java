3b290b897bd26719427b753bb33084a1ec092935
CarterLankford <calankford@outlook.com>
2019-03-15 16:26:54 -0500

    finished if else statment and decided to try switch case, switch case looks better and takes less room

--- TowerOfHanoi/Launcher.java
+++ TowerOfHanoi/Launcher.java
@@ -38,60 +38,52 @@ public class Launcher{
 
 
 					//This is really ugly and I regret everything. Wanted to make it work before it was pretty/reworked
+					// If version
+					/*
 					if (fromOriginalArray == 'a' && toTargetArray == 'b') {
-						System.out.println("a to b");
+						// System.out.println("a to b");
+						// if(!aStack.isEmpty()){
+						// 	if (bStack.isEmpty()){
+						// 		bStack.push(aStack.peek());
+						// 		aStack.pop();
+						// 		currentTurn++;
+						// 	} else if (aStack.peek() < bStack.peek()) {
+						// 		bStack.push(aStack.peek());
+						// 		aStack.pop();
+						// 		currentTurn++;
+						// 	} else {
+						// 		System.out.println("Invalid Move");
+						// 	}
+						// } else {
+						// 	System.out.println("Invalid Move");
+						// }
+					} else if (fromOriginalArray == 'a' && toTargetArray == 'c'){
+						System.out.println("a to c");
 						if(!aStack.isEmpty()){
-							if (bStack.isEmpty()){
-								bStack.push(aStack.peek());
+							if (cStack.isEmpty()){
+								cStack.push(aStack.peek());
 								aStack.pop();
-
-								System.out.println("Towers of Hanoi");
-								System.out.println("========================================");
-								System.out.println(aStack.toString() + "\n" + bStack.toString() + "\n" + cStack.toString());
 								currentTurn++;
-							} else if (aStack.peek() < bStack.peek()) {
-								bStack.push(aStack.peek());
+							} else if (aStack.peek() < cStack.peek()) {
+								cStack.push(aStack.peek());
 								aStack.pop();
-
-								System.out.println("Towers of Hanoi");
-								System.out.println("========================================");
-								System.out.println(aStack.toString() + "\n" + bStack.toString() + "\n" + cStack.toString());
 								currentTurn++;
 							} else {
 								System.out.println("Invalid Move");
 							}
-
-							// System.out.println(aStack.peek() + " " + bStack.peek());
-							// bStack.push(aStack.peek());
-							// aStack.pop();
-
-							// System.out.println("Towers of Hanoi");
-							// System.out.println("========================================");
-							// System.out.println(aStack.toString() + "\n" + bStack.toString() + "\n" + cStack.toString());
-							// currentTurn++;
 						} else {
 							System.out.println("Invalid Move");
 						}
-					} else if (fromOriginalArray == 'a' && toTargetArray == 'c'){
-						System.out.println("a to c");
 					} else if (fromOriginalArray == 'b' && toTargetArray == 'a'){
 						System.out.println("b to a");
 						if(!bStack.isEmpty()){
 							if (aStack.isEmpty()){
 								aStack.push(bStack.peek());
 								bStack.pop();
-
-								System.out.println("Towers of Hanoi");
-								System.out.println("========================================");
-								System.out.println(aStack.toString() + "\n" + bStack.toString() + "\n" + cStack.toString());
 								currentTurn++;
 							} else if (bStack.peek() < aStack.peek()) {
 								aStack.push(bStack.peek());
 								bStack.pop();
-
-								System.out.println("Towers of Hanoi");
-								System.out.println("========================================");
-								System.out.println(aStack.toString() + "\n" + bStack.toString() + "\n" + cStack.toString());
 								currentTurn++;
 							} else {
 								System.out.println("Invalid Move");
@@ -99,23 +91,131 @@ public class Launcher{
 						}
 					} else if (fromOriginalArray == 'b' && toTargetArray == 'c'){
 						System.out.println("b to c");
+						if(!bStack.isEmpty()){
+							if (cStack.isEmpty()){
+								cStack.push(bStack.peek());
+								bStack.pop();
+								currentTurn++;
+							} else if (bStack.peek() < cStack.peek()) {
+								cStack.push(bStack.peek());
+								bStack.pop();
+								currentTurn++;
+							} else {
+								System.out.println("Invalid Move");
+							}
+						} else {
+							System.out.println("Invalid Move");
+						}
 					} else if (fromOriginalArray == 'c' && toTargetArray == 'a'){
 						System.out.println("c to a");
+						if(!cStack.isEmpty()){
+							if (aStack.isEmpty()){
+								aStack.push(cStack.peek());
+								cStack.pop();
+								currentTurn++;
+							} else if (cStack.peek() < aStack.peek()) {
+								aStack.push(cStack.peek());
+								cStack.pop();
+								currentTurn++;
+							} else {
+								System.out.println("Invalid Move");
+							}
+						} else {
+							System.out.println("Invalid Move");
+						}
 					} else if (fromOriginalArray == 'c' && toTargetArray == 'b'){
 						System.out.println("c to b");
+						if(!cStack.isEmpty()){
+							if (bStack.isEmpty()){
+								bStack.push(cStack.peek());
+								cStack.pop();
+								currentTurn++;
+							} else if (cStack.peek() < bStack.peek()) {
+								bStack.push(cStack.peek());
+								cStack.pop();
+								currentTurn++;
+							} else {
+								System.out.println("Invalid Move");
+							}
+						} else {
+							System.out.println("Invalid Move");
+						}
 					} else {
 						System.out.println("Invalid move");
+					}
+					*/
+
+					// Case Version - with logic corrections that aren't in the if version
+					switch (fromOriginalArray){
+						case 'a':
+							if(toTargetArray == 'b' && !aStack.isEmpty()){
+								if (bStack.isEmpty() || aStack.peek() < bStack.peek()) {
+									bStack.push(aStack.peek());
+									aStack.pop();	
+								}								
+							} else if(toTargetArray == 'c' && !aStack.isEmpty()){
+								if (cStack.isEmpty() || aStack.peek() < cStack.peek()) {
+									cStack.push(aStack.peek());
+									aStack.pop();	
+								}
+							} else {
+								System.out.println("False");
+							}
+							currentTurn++;
+							break;
+						case 'b':
+							if(toTargetArray == 'c' && !bStack.isEmpty()){
+								if (cStack.isEmpty() || bStack.peek() < cStack.peek()) {
+									cStack.push(bStack.peek());
+									bStack.pop();
+								}
+							} else if(toTargetArray == 'a' && !bStack.isEmpty()){
+								if (aStack.isEmpty() || bStack.peek() < aStack.peek()) {
+									aStack.push(bStack.peek());
+									bStack.pop();
+								}
+							} else {
+								System.out.println("False");
+							}
+							currentTurn++;
+							break;
+						case 'c':
+							if(toTargetArray == 'a' && !cStack.isEmpty()){
+								if (aStack.isEmpty() || cStack.peek() < aStack.peek()) {
+									aStack.push(cStack.peek());
+									cStack.pop();	
+								}
+							} else if(toTargetArray == 'b' && !cStack.isEmpty()){
+								if (bStack.isEmpty() || cStack.peek() < bStack.peek()) {
+									bStack.push(cStack.peek());
+									cStack.pop();	
+								}
+							} else {
+								System.out.println("False");
+							}
+							break;
+
+						default:
+							System.out.println("False");
+							break;
 					}
 					
 				} 
 
 				if (x.equals("check")){
-					System.out.println("aStack::\t" + aStack.size() + " " + aStack.isEmpty() + "\n" + 
-									   "bStack::\t" + bStack.size() + " " + bStack.isEmpty() );
+					System.out.println("aStack::\t" + aStack.size() + " " + aStack.isEmpty() + "\n" +
+									   "bStack::\t" + bStack.size() + " " + bStack.isEmpty() + "\n" + 
+									   "cStack::\t" + cStack.size() + " " + cStack.isEmpty() );
 				}
 
-
+				// System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+				System.out.println("Towers of Hanoi");
+				System.out.println("========================================");
+				System.out.println("A:" + aStack.toString() + "\n" + "B:" + bStack.toString() + "\n" + "C:" + cStack.toString());
 				currentTurn++;
+
+
+
 			} else {
 				for (int i = 0; i < plateCount; i++){
 					aStack.push(plateCount - i);
