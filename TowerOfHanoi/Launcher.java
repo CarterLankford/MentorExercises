da021bfcc22cfb7dbf1ae8d7704eaac52d235913
CarterLankford <calankford@outlook.com>
2019-03-27 22:07:55 -0500

    Updated to use space rather than tab indenting, and added error handling and worked on other notes from mentor meeting

--- TowerOfHanoi/Launcher.java
+++ TowerOfHanoi/Launcher.java
@@ -1,207 +1,121 @@
 import java.util.Scanner;
 
 public class Launcher{
-	private String[] choices;
-	public static void main(String[] args){
-		boolean recivedExitCode = false;
-		Scanner sc = new Scanner(System.in);
-		String x = "";
-		int currentTurn = 0;
+    private String[] choices;
+    public static void main(String[] args){
+        boolean recivedExitCode = false;
+        Scanner sc = new Scanner(System.in);
+        String x = "";
+        int currentTurn = 0;
 
-		
-		System.out.print("Plate Count: ");
-		int plateCount = sc.nextInt();
-		ArrayStack aStack = new ArrayStack(plateCount);
-		ArrayStack bStack = new ArrayStack(plateCount);
-		ArrayStack cStack = new ArrayStack(plateCount);
+        //Game set up
+        //TODO: Make set up class; currnetly am able to enter 0 or 1 plates, needs minimum of 3
+        System.out.print("Plate Count: ");
+        int plateCount = sc.nextInt();
+        sc.nextLine();
 
-		while(!x.equals("exit")){ // equalsIgnoreCase
-			if (currentTurn > 0){
-				//while looping the game magic happens here.
-				//at the beginning of the loop check to see if player won. 
-				
-				if((bStack.isFull()) || (cStack.isFull())){
-					System.out.println("Game over you won in " + currentTurn + " moves.");
-					break;
-				}
-
-				x = sc.nextLine(); // Looks for next user entry
-				char[] merp = x.toCharArray();
-				char fromOriginalArray;
-				char toTargetArray;
-				int valueToMove = 0;
-
-				//get it to work first, then optimize/refactor it
-				if (merp.length == 3){
-					fromOriginalArray = (char)Sanitize(merp[0]);
-					toTargetArray = (char)Sanitize(merp[2]);
-
-					// VYH: Add a function that receives fromStack toStack:
-
-					/*
-						boolean move(Stack fromStack, Stack toStack){
-							boolean result = false;
-							if(
-							!fromStack.isEmpty() 
-							&& (toStack.isEmpty() || fromStack.peek() < toStack.peek() )
-							){
-									toStack.push(fromStack.pop());
-									result = true;
-								}
-							}
-							return result;
-						}
-
-					Stack fromStack;
-					Stack toStack;
-
-					switch(fromOriginalArray){
-						case 'a':
-							fromStack = aStack;
-							break;
-						case 'b':
-							fromStack = bStack;
-							break;
-						case 'c':
-							fromStack = cStack;
-							break;
-						default:
-							// TODO: add some error handling here
-					}
-
-					switch(toTargetArray){
-						case 'a':
-							toStack = aStack;
-							break;
-						case 'b':
-							toStack = bStack;
-							break;
-						case 'c':
-							toStack = cStack;
-							break;
-						default:
-							// TODO: add some error handling here	
-					}
-
-					System.out.println(move(fromStack, toStack));
-					*/
-
-					switch (fromOriginalArray){
-						case 'a':
-							if(toTargetArray == 'b' && !aStack.isEmpty()){
-								if (bStack.isEmpty() || aStack.peek() < bStack.peek()) {
-									bStack.push(aStack.peek());
-									aStack.pop();
-
-									// bStack.push(aStack.pop()); ~ VYH	
-								}								
-							} else if(toTargetArray == 'c' && !aStack.isEmpty()){
-								if (cStack.isEmpty() || aStack.peek() < cStack.peek()) {
-									cStack.push(aStack.peek());
-									aStack.pop();	
-								}
-							} else {
-								System.out.println("False");
-							}
-							currentTurn++;
-							break;
-						case 'b':
-							if(toTargetArray == 'c' && !bStack.isEmpty()){
-								if (cStack.isEmpty() || bStack.peek() < cStack.peek()) {
-									cStack.push(bStack.peek());
-									bStack.pop();
-								}
-							} else if(toTargetArray == 'a' && !bStack.isEmpty()){
-								if (aStack.isEmpty() || bStack.peek() < aStack.peek()) {
-									aStack.push(bStack.peek());
-									bStack.pop();
-								}
-							} else {
-								System.out.println("False");
-							}
-							currentTurn++;
-							break;
-						case 'c':
-							if(toTargetArray == 'a' && !cStack.isEmpty()){
-								if (aStack.isEmpty() || cStack.peek() < aStack.peek()) {
-									aStack.push(cStack.peek());
-									cStack.pop();	
-								}
-							} else if(toTargetArray == 'b' && !cStack.isEmpty()){
-								if (bStack.isEmpty() || cStack.peek() < bStack.peek()) {
-									bStack.push(cStack.peek());
-									cStack.pop();	
-								}
-							} else {
-								System.out.println("False");
-							}
-							break;
-
-						default:
-							System.out.println("False");
-							break;
-					}
-					
-				} 
-
-				if (x.equals("check")){
-					System.out.println("aStack::\t" + aStack.size() + " " + aStack.isEmpty() + "\n" +
-									   "bStack::\t" + bStack.size() + " " + bStack.isEmpty() + "\n" + 
-									   "cStack::\t" + cStack.size() + " " + cStack.isEmpty() );
-				}
-
-				// System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-				System.out.println("Towers of Hanoi");
-				System.out.println("========================================");
-				System.out.println("A:" + aStack.toString() + "\n" + "B:" + bStack.toString() + "\n" + "C:" + cStack.toString());
-				currentTurn++;
-
-			} else {
-				// VYH: All of this is game setup, should happen before the while
-				for (int i = 0; i < plateCount; i++){
-					aStack.push(plateCount - i);
-				}
-
-				// VYH: Write a function to print all towers: printTowers()
-				System.out.println("Towers of Hanoi");
-				System.out.println("========================================");
-				System.out.println(aStack.toString() + "\n" + bStack.toString() + "\n" + cStack.toString());
-				currentTurn++;
-			}
-		}
-
-		//TODO: 
-		// [] Gather from user how many plates int plateCount ~ VYH: done
-		// [] create the 3 arrays ABC and pass them the count variable
-		// [] Load A with all the plates
-		// [] Print the first screen
-		// [] Check to see if player won
-	}
-
-	private static int Sanitize(int inputChar){
-		//To return lower case letter, 0-9, or -1
-		int outputChar = inputChar;
-		if (inputChar >= 65 && inputChar <= 67 || inputChar >= 97 && inputChar <= 99) {
-			if(inputChar >= 65 && inputChar <= 67){
-				//From Upper to Lower
-				outputChar = inputChar + 32;
-			} else if (inputChar >= 97 && inputChar <= 99) {
-				//Returns lower or 0-9
-				outputChar = inputChar;
-			} else {
-				//All other char will return -1
-				outputChar = -1;
-			}
-
-			return outputChar;
-		} else {
-			return -1;
-		}
-	}
-
-	private static boolean MoveValue(char fromOriginalArray, char toTargetArray){
+        Stack[] stacks = new Stack[3];
+        stacks[0] = new ArrayStack(plateCount);
+        stacks[1] = new ArrayStack(plateCount);
+        stacks[2] = new ArrayStack(plateCount);
 
 
-		return true;
-	}
+        for (int i = 0; i < plateCount; i++){
+            stacks[0].push(plateCount - i);
+        }
+
+        System.out.println("Towers of Hanoi");
+        System.out.println("========================================");
+        printBoard(stacks);
+        
+
+        while(!x.equalsIgnoreCase("exit")){ // equalsIgnoreCase            
+            //while looping the game magic happens here.
+            //at the beginning of the loop check to see if player won. 
+                
+            if((stacks[1].isFull()) || (stacks[2].isFull())){
+                System.out.println("Game over you won in " + currentTurn + " moves.");
+                break;
+            }
+
+            System.out.print("> ");
+            System.out.flush();
+            x = sc.nextLine(); // Looks for next user entry
+
+            char[] merp = x.toCharArray();
+            int fromOriginalArray;
+            int toTargetArray;
+            int valueToMove = 0;
+
+            // VYH: Validate your input over the first 3 chars, ignore anything else after that ~ CAL: done
+            // validate that the actual input was # # (char space char) ~ CAL: done
+            try {
+                if (merp.length >= 3) {
+                // VYH: Sanitize should return an int 0 1 or 2 if a valid character was entered ~ CAL: done
+                // VYH: Make Sanitize throw an exception if input is not valid ~ CAL: done
+                // VYH: catch exception here and move forward with next iteration ~ CAL: done
+
+                try {
+                    fromOriginalArray = Sanitize(merp[0]);
+                    toTargetArray = Sanitize(merp[2]);
+                    System.out.println(moveMe(stacks[fromOriginalArray], stacks[toTargetArray]) + "\n\n");
+                    currentTurn++;
+                } catch (IllegalArgumentException exception){
+                    exception.printStackTrace();
+                } finally {
+                    System.out.println("\n\n");
+                    printBoard(stacks);
+                    continue;                        
+                }                      
+                } else if (x.equals("check")){
+                    System.out.println("aStack::\t" + stacks[0].size() + " " + stacks[0].isEmpty() + "\n" +
+                                       "bStack::\t" + stacks[1].size() + " " + stacks[1].isEmpty() + "\n" + 
+                                       "cStack::\t" + stacks[2].size() + " " + stacks[2].isEmpty() );
+                } else {
+                throw new IllegalArgumentException("Must enter two values seperated by a space.");
+                }              
+            } catch (IllegalArgumentException exception) {
+                exception.printStackTrace();
+                System.out.println("\n\n");
+                printBoard(stacks);
+            } finally {
+                continue;
+            }
+
+            
+        }
+    }
+
+    private static int Sanitize(int inputChar){
+        //to return 0-3 
+        int outputChar = inputChar;
+        if (inputChar >= 65 && inputChar <= 67 || inputChar >= 97 && inputChar <= 99) {
+            if(inputChar >= 65 && inputChar <= 67){
+                //Upercase to 0-3
+                return outputChar = inputChar - 65;
+            } else if (inputChar >= 97 && inputChar <= 99) {
+                //lower case to 0-3
+                return outputChar = inputChar - 97;
+            } else {
+                throw new IllegalArgumentException("Incorrect value entered");
+            }
+        } else {
+            throw new IllegalArgumentException("Incorrect value entered");
+        }
+    }
+
+    private static void printBoard(Stack[] stacks){
+        System.out.println("A:" + stacks[0].toString() + "\n" + "B:" + stacks[1].toString() + "\n" + "C:" + stacks[2].toString());
+
+    }
+
+    private static boolean moveMe(Stack fromStack, Stack toStack){
+        boolean result = false;
+        if(!fromStack.isEmpty() && (toStack.isEmpty() || fromStack.peek() < toStack.peek())){
+            toStack.push(fromStack.pop());
+            result = true;
+        }
+                            
+        return result;
+    }
 }
