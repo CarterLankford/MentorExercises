6695c314e14f7f0d17e5a6daeef20840eb26c4dc
CarterLankford <calankford@outlook.com>
2019-04-08 21:04:29 -0500

    modified tests, Triangle still needs work, but progress has been made

--- Geometry/Geometry.java
+++ Geometry/Geometry.java
@@ -1,71 +1,41 @@
 public class Geometry{
     public static void main(String[] args) {
         //TODO:
-        //[*] write a test case for everything
-        //[*] rewite test methods to include try/catch
-            //[*] Circle
-            //[*] Regular Polygon
-            //[*] Square
-            //[*] Triangle
-        //[*] add edge cases, min, max, med, negitive
-        //[*] Add error handling to RegularPolygon
-        //[*] Add error handling to AbstractTriangle
-
-        
+        //[*] Correct test cases for Circle
+        //[] Correct test cases for Triangle
 
         
         //RegularPolygon
-        // unitTestingRegularPolygon((float)-4, -5, 0f, 0f, 0f);
-        // unitTestingRegularPolygon((float)1, 1, 0f, 0f, 0f);
-        // unitTestingRegularPolygon((float)6, 7, 42f, 135.10394f, 6.433521f);
-        // unitTestingRegularPolygon((float)900, 900, 2f, 2f, 2f);
+        unitTestingRegularPolygon((float)-4, -5, 0f, 0f, 0f);
+        unitTestingRegularPolygon((float)1, 1, 0f, 0f, 0f);
+        unitTestingRegularPolygon((float)6, 7, 42f, 135.10394f, 6.433521f);
+        unitTestingRegularPolygon((float)90, 90, 8100f, 5218957f, 1288.6313f);
 
         
         // Square
         unitTestingSquare((float)-1, -1, 0f, 0f);
         unitTestingSquare((float)1, 1, 4f, 1f);
-        unitTestingSquare((float)20, 20, 0f, 0f);
-        unitTestingSquare((float)900, 900, 0f, 0f);
+        unitTestingSquare((float)20, 20, 80f, 400f);
+        unitTestingSquare((float)900, 900, 3600f, 810000f);
 
-        /*
+        
         //Circle
-        unitTestingCircle((float)-1);
-        unitTestingCircle((float)1);
-        unitTestingCircle((float)20);
-        unitTestingCircle((float)900);
-        // VYH unitTestingCircle((float) 900, resultOfArea, resultOfPerimeter)
+        unitTestingCircle((float)-1, 5f, 5f, 5f);
+        unitTestingCircle((float)1, 6.28318f, 3.14159f, 2f);
+        unitTestingCircle((float)20, 125.663605f, 1256.636f, 40f);
+        unitTestingCircle((float)900, 5654.8623f, 2544688f, 1800f);
+
 
         //Triangle
-        unitTestingTriangle((float)20, (float)10, (float)20, 80, 80, 30);
-        unitTestingTriangle((float)-20, (float)-10, (float)-20, -80, -80, -30); 
-        */
+        unitTestingTriangle((float)20, (float)10, (float)20, 80, 80, 30, 52.36068f, 100f);
+        unitTestingTriangle((float)1, (float)1, (float)1, 1, 1, 1, 1f, 1f);
+        unitTestingTriangle((float)-1, (float)-1, (float)-1, -1, -1, -1, 1f, 1f);
+
+        
     }
 
     private static void displayPerimteter(Shape anyShape){
         System.out.println("Perimeter of " + anyShape.getClass().getName() + " is " + anyShape.getPerimeter());
-    }
-
-    private static void merpUnitTest(float lengthOfSide, int numberOfSides, float expectedPerimeter, float expectedArea, float expectedApothem){
-        try {
-            boolean passTest = false;
-            RegularPolygon rp = new RegularPolygon();
-            rp.setLengthOfSide((float)lengthOfSide);
-            rp.setNumberOfSides(numberOfSides);
-
-            if (rp.getPerimeter() == expectedPerimeter && rp.getArea() == expectedArea && rp.getApothem() == expectedApothem){
-                passTest = !passTest;
-            } 
-
-            System.out.println("\nmerpUnitTest(float lengthOfSide,int numberOfSides)\n" 
-                                + "lengthOfSide: " + lengthOfSide + " numberOfSides: " + numberOfSides + "\n"
-                                + "Expected Results:\n\tPerimeter: " + expectedPerimeter + "\tArea: " + expectedArea + "\tApothem: " + expectedApothem + "\n"
-                                + "Actual Results:\n\tPerimeter: " + rp.getPerimeter() + "\tArea: " + rp.getArea() + "\tApothem: " + rp.getApothem() + "\n"
-                                + "Pass Test = " + passTest + "\n");
-        } catch (IllegalArgumentException exception){
-            System.out.println("\nmerpUnitTest(float lengthOfSide, int numberOfSides)\nPass Test = false, execption thrown\n");
-        }
-
-        
     }
 
     private static void unitTestingRegularPolygon(float lengthOfSide, int numberOfSides, float expectedPerimeter, float expectedArea, float expectedApothem) {
@@ -106,7 +76,7 @@ public class Geometry{
         }
     }
 
-    private static void unitTestingSquare(float lengthOfSide, int numberOfSides, float expectedArea, float expectedPerimeter){
+    private static void unitTestingSquare(float lengthOfSide, int numberOfSides, float expectedPerimeter, float expectedArea){
         //Square()
         try{
             boolean passTest = false;
@@ -127,102 +97,158 @@ public class Geometry{
         //Square((float)lengthOfSide)
         try{
             //TODO: add this
+            boolean passTest = false;
+            System.out.println("Square((float)lengthOfSide)\nInput Values(lengthOfSide: " + lengthOfSide + ", numberOfSides:" + numberOfSides + ")");
+            Square sqa = new Square((float)lengthOfSide);
+            sqa.setNumberOfSides(numberOfSides);
+            if (sqa.getPerimeter() == expectedPerimeter && sqa.getArea() == expectedArea){
+                passTest = true;
+            }
+
+            System.out.println("Expected Results:\n\tPerimeter: " + expectedPerimeter + "\tArea: " + expectedArea + "\n"
+                                + "Actual Results:\n\tPerimeter: " + sqa.getPerimeter() + "\tArea: " + sqa.getArea() + "\n"
+                                + "Pass Test = " + passTest + "\n");
+        } catch (IllegalArgumentException exception){
+            System.out.println("Pass Test = false, execption thrown\n");
+        }
+    }
+
+    private static void unitTestingCircle(float radius, float expectedPerimeter, float expectedArea, float expectedDiameter){
+        //Circle()
+        try{
+            boolean passTest = false;
+            System.out.println("Circle()\nInput Values(Circle.setRadius: " + radius + ")");
+            Circle cir = new Circle();
+            cir.setRadius((float)radius);
+            if(cir.getPerimeter() == expectedPerimeter && cir.getArea() == expectedArea && cir.getDiameter() == expectedDiameter){
+                passTest = true;
+            }
+
+            System.out.println("Expected Results:\n\tPerimeter: " + expectedPerimeter + "\tArea: " + expectedArea + "\tDiameter: " + expectedDiameter + "\n"
+                                + "Actual Results:\n\tPerimeter: " + cir.getPerimeter() + "\tArea: " + cir.getArea() + "\tDiameter: " + cir.getDiameter() + "\n"
+                                + "Pass Test = " + passTest + "\n");
+
         } catch (IllegalArgumentException exception){
             System.out.println("Pass Test = false, execption thrown\n");
         }
 
+        //Circle((float)radius)
+        try{
+            boolean passTest = false;
+            System.out.println("Circle((float)radius)\nInput Values(Radius: " + radius + ")");
+            Circle cir2 = new Circle(radius);
+            if(cir2.getPerimeter() == expectedPerimeter && cir2.getArea() == expectedArea && cir2.getDiameter() == expectedDiameter){
+                passTest = true;
+            }
 
-        // System.out.println("Square()");
-        // try {
-        //     Square sq = new Square();
-        //     sq.setLengthOfSide((float)lengthOfSide);
-        //     System.out.println("Length of side: " + sq.getLengthOfSide() + " | " + "Number of Sides: " + sq.getNumberOfSides() + "\n" +
-        //                        "Area: " + sq.getArea() + " | " + "Perimeter: " + sq.getPerimeter() + "\n");
+            System.out.println("Expected Results:\n\tPerimeter: " + expectedPerimeter + "\tArea: " + expectedArea + "\tDiameter: " + expectedDiameter + "\n"
+                                + "Actual Results:\n\tPerimeter: " + cir2.getPerimeter() + "\tArea: " + cir2.getArea() + "\tDiameter: " + cir2.getDiameter() + "\n"
+                                + "Pass Test = " + passTest + "\n");
 
-        // } catch (IllegalArgumentException exception) {
-        //     exception.printStackTrace();
-        // }
 
-        // System.out.println("Square((float)lengthOfSide)");
-        // try {
-        //     Square sqa = new Square((float)lengthOfSide);
-        //     System.out.println("Length of side: " + sqa.getLengthOfSide() + " | " + "Number of Sides: " + sqa.getNumberOfSides() + "\n" +
-        //                        "Area: " + sqa.getArea() + "\n" + "Perimeter: " + sqa.getPerimeter() + "\n");
-
-        //     sqa.setNumberOfSides(numberOfSides);
-        //     System.out.println("Square: input of length of sides after calling setNumberOfSides(" + numberOfSides + ")");
-        //     System.out.println("Length of side: " + sqa.getLengthOfSide() + " | " + "Number of Sides: " + sqa.getNumberOfSides() + "\n" +
-        //                        "Area: " + sqa.getArea() + " | " + "Perimeter: " + sqa.getPerimeter() + "\n");
-
-        // } catch (IllegalArgumentException exception) {
-        //     exception.printStackTrace();
-        // }
-
+        } catch (IllegalArgumentException exception){
+            System.out.println("Pass Test = false, execption thrown\n");
+        }
     }
 
-    private static void unitTestingCircle(float radius){
-        //Circle()
-        System.out.println("Circle()");
+    //TODO: I see now each one of these should have their own test and not be lumped togheter. Need to rewrite. 
+    private static void unitTestingTriangle(float s1, float s2, float s3, int a1, int a2, int a3, float expectedPerimeter, float expectedArea){
+        //RightTriangle((float)s1, (float)s2, int a1)
         try {
-            Circle cir = new Circle();
-            cir.setRadius((float)radius);
-            // float per = cir.getPerimeter();
-            // if(per !== expectedPerimeter){
-            //   display error
-            // }
-            System.out.println("Radius: " + cir.getRadius() + " | " + "Diameter: " + cir.getDiameter() + "\n" +
-                               "PI: " + cir.getPI() + " | " + "Area: " + cir.getArea() + " | " + "Perimeter: " + cir.getPerimeter() + "\n" +
-                               "Number of Instances: " + cir.getNumberOfInstances() + "\n");
+            boolean passTest = false;
+            System.out.println("RightTriangle((float)s1, (float)s2, int a1)\nInput Values(s1: " + s1 + ", s2: " + s2 + ", a1: " + a1 + ")");
+            AbstractTriangle rt =  new RightTriangle((float)s1, (float)s2, (int)a1);
+            if(rt.getPerimeter() == expectedPerimeter && rt.getArea() == expectedArea){
+                passTest = true;
+            }
 
-        } catch (IllegalArgumentException exception) {
+            System.out.println("Expected Results:\n\tPerimeter: " + expectedPerimeter + "\tArea: " + expectedArea + "\n"
+                                + "Actual Results:\n\tPerimeter: " + rt.getPerimeter() + "\tArea: " + rt.getArea() + "\n"
+                                + "Pass Test = " + passTest + "\n");
+
+
+        } catch (IllegalArgumentException exception){
             exception.printStackTrace();
+        } finally{
+            System.out.println(" ");
         }
 
-        //Circle((float)radius)
-        System.out.println("Circle((float) radius)");
+        //IsoscelesTriangle((float)s1, (float)s2, int a1)
         try {
-            Circle cir2 = new Circle((float)radius);            
-            System.out.println("Radius: " + cir2.getRadius() + " | " + "Diameter: " + cir2.getDiameter() + "\n" +
-                               "PI: " + cir2.getPI() + " | " + "Area: " + cir2.getArea() + "\n" +
-                               "Perimeter: " + cir2.getPerimeter() + "\n");
-        } catch (IllegalArgumentException exception) {
+            boolean passTest = false;
+            System.out.println("IsoscelesTriangle((float)s1, (float)s2, int a1)\nInput Values(s1: " + s1 + ", s2: " + s2 + ", a1: " + a1 + ")");
+            AbstractTriangle ist =  new IsoscelesTriangle((float)s1, (float)s2, (int)a1);
+            if(ist.getPerimeter() == expectedPerimeter && ist.getArea() == expectedArea){
+                passTest = true;
+            }
+
+            System.out.println("Expected Results:\n\tPerimeter: " + expectedPerimeter + "\tArea: " + expectedArea + "\n"
+                                + "Actual Results:\n\tPerimeter: " + ist.getPerimeter() + "\tArea: " + ist.getArea() + "\n"
+                                + "Pass Test = " + passTest + "\n");
+
+
+        } catch (IllegalArgumentException exception){
             exception.printStackTrace();
-        }      
-    }
+        } finally{
+            System.out.println(" ");
+        }
 
-    private static void unitTestingTriangle(float s1, float s2, float s3, int a1, int a2, int a3){
-      // RightTriangle rt = new RightTriangle(s1, s2, a1);
-      // IsoscelesTriangle isot = new IsoscelesTriangle(s1, s2, a1);
-      EquilateralTriangle eqlt = new EquilateralTriangle(s1);
+        // //EquilateralTriangle((float)s1)
+        try {
+            boolean passTest = false;
+            System.out.println("EquilateralTriangle((float)s1)\nInput Values(s1: " + s1 + ")");
+            AbstractTriangle elt =  new EquilateralTriangle((float)s1);
+            if(elt.getPerimeter() == expectedPerimeter && elt.getArea() == expectedArea){
+                passTest = true;
+            }
 
-      try {
-        AbstractTriangle triangle = new IsoscelesTriangle(180f, 180f, 35);
-        System.out.println("This will never be reached");
-      } catch (IllegalArgumentException exception){
-        exception.printStackTrace();
-      } finally {
-        System.out.print("\n");
-      }
+            System.out.println("Expected Results:\n\tPerimeter: " + expectedPerimeter + "\tArea: " + expectedArea + "\n"
+                                + "Actual Results:\n\tPerimeter: " + elt.getPerimeter() + "\tArea: " + elt.getArea() + "\n"
+                                + "Pass Test = " + passTest + "\n");
 
-      System.out.println("Isosceles Triangle:");
-      try {
-        AbstractTriangle isot = new IsoscelesTriangle(s1, s2, a1);
-        System.out.println("Area: " + isot.getArea() + "\tPerimeter: " + isot.getPerimeter());
-      } catch (IllegalArgumentException exception){
-        exception.printStackTrace();
-      } finally {
-        System.out.print("\n");
-      }
 
-      System.out.println("Right Triangle" + "\tInput: \ts1:" + s1 + " \ts2:" + s2 + " \ta1:" + a1);
-      try {
-        AbstractTriangle rt = new RightTriangle(s1, s2, a1);
-        System.out.println("Area: " + rt.getArea() + "\tPerimeter: " + rt.getPerimeter());
-      } catch (IllegalArgumentException exception){
-        exception.printStackTrace();
-      } finally {
-        System.out.print("\n");
-      }
-      System.out.println("EquilateralTriangle: \tArea: " + eqlt.getArea () + "\tPerimeter: " + eqlt.getPerimeter());      
+        } catch (IllegalArgumentException exception){
+            exception.printStackTrace();
+        } finally{
+            System.out.println(" ");
+        }
+
+
+
+
+
+      // // RightTriangle rt = new RightTriangle(s1, s2, a1);
+      // // IsoscelesTriangle isot = new IsoscelesTriangle(s1, s2, a1);
+      // EquilateralTriangle eqlt = new EquilateralTriangle(s1);
+
+      // try {
+      //   AbstractTriangle triangle = new IsoscelesTriangle(180f, 180f, 35);
+      //   System.out.println("This will never be reached");
+      // } catch (IllegalArgumentException exception){
+      //   exception.printStackTrace();
+      // } finally {
+      //   System.out.print("\n");
+      // }
+
+      // System.out.println("Isosceles Triangle:");
+      // try {
+      //   AbstractTriangle isot = new IsoscelesTriangle(s1, s2, a1);
+      //   System.out.println("Area: " + isot.getArea() + "\tPerimeter: " + isot.getPerimeter());
+      // } catch (IllegalArgumentException exception){
+      //   exception.printStackTrace();
+      // } finally {
+      //   System.out.print("\n");
+      // }
+
+      // System.out.println("Right Triangle" + "\tInput: \ts1:" + s1 + " \ts2:" + s2 + " \ta1:" + a1);
+      // try {
+      //   AbstractTriangle rt = new RightTriangle(s1, s2, a1);
+      //   System.out.println("Area: " + rt.getArea() + "\tPerimeter: " + rt.getPerimeter());
+      // } catch (IllegalArgumentException exception){
+      //   exception.printStackTrace();
+      // } finally {
+      //   System.out.print("\n");
+      // }
+      // System.out.println("EquilateralTriangle: \tArea: " + eqlt.getArea () + "\tPerimeter: " + eqlt.getPerimeter());      
     }
 }
