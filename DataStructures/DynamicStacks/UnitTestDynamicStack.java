c0e5ef07199eeefe15402d60881cbe930dc84e05
CarterLankford <calankford@outlook.com>
2019-04-24 10:52:20 -0500

    Added validation function for testing

--- DataStructures/DynamicStacks/UnitTestDynamicStack.java
+++ DataStructures/DynamicStacks/UnitTestDynamicStack.java
@@ -18,8 +18,30 @@ public class UnitTestDynamicStack{
 
 
         Node nd = new Node()
-            .withPayload(5)
-            .withNext(null);
+            .withPayload(5);
+
+        TestWithNode(nd, 5);
+
+        Node nd2 = new Node().withPayload(3).withNext(nd);
+        TestWithNode(nd2, 3);
+
+
+
+        // Node nd = new Node().withPayload(5);
+        // if(nd.getNext() == null) {
+        //     System.out.println("No next available");
+        // }
+
+
+
+
+        // Node nd = new Node()
+        //     .withPayload(5)
+        //     .withNext(null);
+
+        // Node nd2 = new Node()
+        //     .withPayload(3)
+        //     .withNext(nd);
 
     }
 
@@ -40,5 +62,30 @@ public class UnitTestDynamicStack{
         for (int i=0; i <= numberOut; i++){
             System.out.println("Empty-" + x.isEmpty() + " | Current Size-" + x.size() + " | Value Removed-" + x.pop() + " | Peek-" + x.peek() + " | Full-" + x.isFull() + " | Node Count-" + x.size());
         }
+    }
+
+    public static void TestWithNode(Node node, int payload){
+        Node x = node;
+        String outputPart1 = new String();
+        String outputPart2 = new String();
+
+        if(x.getNext() == null){
+            outputPart1 = "No next available";
+            // System.out.println("No next available");
+        } else {
+            // System.out.println("Next is available");
+            outputPart1 = "Next is available";
+        }
+
+        if(x.getPayload() == payload){
+            // System.out.println("payload available, and is equal to " + payload);
+            outputPart2 = "payload available and is equal to " + payload;
+        } else {
+            // System.out.println("Payload doen't equal expected value");
+            outputPart2 = "Payload doen't equal expected value";
+        }
+
+        System.out.println(outputPart1 + ", " + outputPart2);
+
     }
 }
