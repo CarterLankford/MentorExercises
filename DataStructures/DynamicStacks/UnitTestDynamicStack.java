32c89e4079eda2621ad198a47b771236c416c9ef
CarterLankford <calankford@outlook.com>
2019-05-17 15:40:40 -0500

    adjusted test to look for node id and payload. Added comments and removed strings as reqeusted

--- DataStructures/DynamicStacks/UnitTestDynamicStack.java
+++ DataStructures/DynamicStacks/UnitTestDynamicStack.java
@@ -20,29 +20,19 @@ public class UnitTestDynamicStack{
         Node nd = new Node()
             .withPayload(5);
 
-        TestWithNode(nd, 5);
+        TestWithNode(nd, 5, null, null); //True
 
-        Node nd2 = new Node().withPayload(3).withNext(nd);
-        TestWithNode(nd2, 3);
+        Node nd2 = new Node()
+            .withPayload(3)
+            .withNext(nd);
 
+        TestWithNode(nd2, 3, nd, 5); //True
+        
+        Node nd3 = new Node()
+            .withPayload(9)
+            .withNext(nd2);
 
-
-        // Node nd = new Node().withPayload(5);
-        // if(nd.getNext() == null) {
-        //     System.out.println("No next available");
-        // }
-
-
-
-
-        // Node nd = new Node()
-        //     .withPayload(5)
-        //     .withNext(null);
-
-        // Node nd2 = new Node()
-        //     .withPayload(3)
-        //     .withNext(nd);
-
+        TestWithNode(nd3, 9, nd2, 20); //False       
     }
 
     public static void TestDynamicStack(int numberIn, int numberOut, DynamicStack nameOfStack){
@@ -64,28 +54,43 @@ public class UnitTestDynamicStack{
         }
     }
 
-    public static void TestWithNode(Node node, int payload){
-        Node x = node;
-        String outputPart1 = new String();
-        String outputPart2 = new String();
-
-        if(x.getNext() == null){
-            outputPart1 = "No next available";
-            // System.out.println("No next available");
+    public static void TestWithNode(Node currentNode, int currentExpectedPayload, Node expectedNext, Integer expectedNextPayload){
+        //Validate current node creation, with expected Payload
+        if(currentNode.getPayload() == currentExpectedPayload) {
+            System.out.println("CurrentNode Created Successfully: True");
         } else {
-            // System.out.println("Next is available");
-            outputPart1 = "Next is available";
+            System.out.println("CurrentNode Created Successfully: False");
         }
 
-        if(x.getPayload() == payload){
-            // System.out.println("payload available, and is equal to " + payload);
-            outputPart2 = "payload available and is equal to " + payload;
+        //Printing what we have vs what we expect
+        System.out.print("Input: CurrentNode = " + currentNode + ", CurrentPayload = " + currentExpectedPayload + " || ExpectedNextNode = " + expectedNext + ", ExpectedNextNodePayload = " + expectedNextPayload + " || ");
+
+        Node nextNode = currentNode.getNext();
+        //If Node is null it will throw an error, so let's check first
+        if (nextNode != null) {
+            Integer nextPayload = nextNode.getPayload();
+
+            //Displays the output to console
+            System.out.print(" Output: NextNode = " + nextNode + ", nextPayload = " + nextPayload + "\n");
+
+            //Checks to see if both node and payload matches the expected, it either matches fully or not at all.
+            if (nextNode.equals(expectedNext) && nextPayload == expectedNextPayload){
+                System.out.println("NextNode Matches expected NextNode: True");                
+            } else {
+                System.out.println("NextNode Matches expected NextNode: False");
+            }
         } else {
-            // System.out.println("Payload doen't equal expected value");
-            outputPart2 = "Payload doen't equal expected value";
+            System.out.print(" Output: NextNode = null, nextPayload = null\n");
+            //If it's expected to be null/null
+            //Checks to see if both node and payload matches the expected, it either matches fully or not at all.
+            if (expectedNext == null && expectedNextPayload == null) {
+                System.out.println("NextNode Matches expected NextNode: True");
+            } else {
+                System.out.println("NextNode Matches expected NextNode: False");
+            }
         }
 
-        System.out.println(outputPart1 + ", " + outputPart2);
-
+        //To keep the output clean        
+        System.out.println("");
     }
 }
