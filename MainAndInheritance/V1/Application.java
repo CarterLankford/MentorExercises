76b82673d10dc7143b27b642f6e05c614033a159
CarterLankford <calankford@outlook.com>
2019-06-19 10:44:34 -0500

    adding comments to reflect what was learned. I might have used incorrect terms, but I feel like I understood the exercise.

--- MainAndInheritance/V1/Application.java
+++ MainAndInheritance/V1/Application.java
@@ -9,22 +9,35 @@ public class Application{
         hijo = new Child();
 
         System.out.println("Grandparent: " + abuelo.greet() + "\nParent: " + padre.greet() + "\nChild: " + hijo.greet());
+        //1.C: Everything shown was expected
+
         System.out.println("Grandparent: " + abuelo.waveOff() + "\nParent: " + padre.waveOff() + "\nChild: " + hijo.waveOff());
+        //1.D: padre's gets it's waveOff from the Grandparent class, since it extends the Grandparent class.
 
         Human george;
         george = new Human();
         System.out.println("George greet: " + george.greet() + "\nGeorge waveOff: " + george.waveOff());
+        //2.C: Everything shown was expected
 
         george = new Child();
+        //2.D: The code does compile. The code can compile since George was initialized as the Human class first.
         System.out.println("George greet: " + george.greet() + "\nGeorge waveOff: " + george.waveOff());
+        //2.E: The methods called are from the Child class, since george was assigned to the child class
 
         george = new Parent();
+        //2.F: The code does compile. Since the Human type is the highest point of inhertance we can assign it to the 
+        //parent class because it extends from the Grandparent class, which extends from the Human class.
         System.out.println("George greet: " + george.greet() + "\nGeorge waveOff: " + george.waveOff());        
+        //2.G The method greet is from the parent class, but the method waveOff is from the grandparent class because
+        //it's inherited from the grandparent class
 
         Child ada;
         ada = new Child();
         System.out.println("Ada greet: " + ada.greet() + "\nAda waveOff: " + ada.waveOff());
+        //2.J: The output is shown as expected.
 
         //ada = new Parent();
+        //2.K: This code doesn't compile. This is because ada was initialized into the child class
+        // and is lowest class in the chain and is unable to be assigned to the others. 
     }
 }
--- MainAndInheritance/V2/Application.java
+++ MainAndInheritance/V2/Application.java
@@ -9,13 +9,27 @@ public class Application{
         aman = new Grandparent();
 
         System.out.println("Vicente Speak: " + vicente.speak() + "\nTrish Speak: " + trish.speak() + "\nAman Speak: " + aman.speak() + "\n");
+        //3.F: Quote was shown as expected in all three cases. Aman get it's speak method from the People class. All three are considered people
+        // since Human class implements from People interface and Grandparent class extends from Human class, Parent class from Grandparent class, 
+        // and Child class from Parent class
 
         People persona;
         persona = vicente;
+        //3.H It does compile since Human class implements from the People interface. 
+
         System.out.println("Persona Speak: " + persona.speak());
-        //System.out.println("Persona Greet: " + persona.greet()); // Won't work because it's People, and people only has speak.
+        //3.I Output is shown as expected. 
+
+        //System.out.println("Persona Greet: " + persona.greet()); 
+        //3.J: This will not complie since persona comes from People which only has a speak method.         
+        
+
         persona = aman;
+        //3.K: This code does compile. 
         System.out.println("Persona Speak: " + persona.speak());
+        //3.L The output is what was expected.
+
+        //Did some extra work for fun/exercise. 
         persona = trish;
         System.out.println("Persona Speak: " + persona.speak());
 
