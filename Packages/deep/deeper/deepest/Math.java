6f80465e530d60c21cdc40aa45cfde1dd9f07a78
CarterLankford <calankford@outlook.com>
2019-07-15 13:27:04 -0500

    adding in work from last session that shows basic usage of packages

--- Packages/deep/deeper/deepest/Math.java
+++ Packages/deep/deeper/deepest/Math.java
@@ -1,9 +1,5 @@
-/**
- * Instructions to compile:
- * ## This might vary for other computers ##
- * 
- */
 package deep.deeper.deepest;
+
 public class Math{
     public static int sum(int a, int b){
         return a + b;
