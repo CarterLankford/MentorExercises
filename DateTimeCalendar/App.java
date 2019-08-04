7082d2c73ebdbaeaefaae7c2f596fa6901bf8a26
Carter Lankford <calankford@outlook.com>
2019-08-04 18:56:51 -0500

    First push on this branch includes a mostly done UML and the base classes as I'm currently planning them

---
+++ DateTimeCalendar/App.java
@@ -0,0 +1,24 @@
+public class App{
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+
+
+
+    // //Below is a demonstration of how to return an array
+    // static int[] getSumAndSub(int a, int b){ 
+    //     int[] ans = new int[2]; 
+    //     ans[0] = a + b; 
+    //     ans[1] = a - b; 
+  
+    //     // returning array of elements 
+    //     return ans; 
+    // } 
+  
+    // // Driver method 
+    // public static void main(String[] args){ 
+    //     int[] ans = getSumAndSub(100, 50); 
+    //     System.out.println("Sum = " + ans[0]); 
+    //     System.out.println("Sub = " + ans[1]); 
+    // } 
+}
